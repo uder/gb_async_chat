c8bf2bacc1c4a889d408f7fb3ee424217814397c
Salnikov <salnikov@100sp.ru>
2021-09-24 16:18:58 +1000

    implementation of task 1 and task 2

---
+++ jimmy/client/sender_client.py
@@ -0,0 +1,15 @@
+from .client import Client
+
+
+class SenderClient(Client):
+    _logname = 'sender_client'
+
+    def start(self):
+        try:
+            self.socket.connect((self.server_addr, self.server_port))
+        except Exception as err:
+            self.logger.error(f'Cant establish connection to {self.server_addr}:{self.server_port}')
+        else:
+            self._send_message(self.message_type, account_name=self.account_name)
+
+            self.socket.close()
