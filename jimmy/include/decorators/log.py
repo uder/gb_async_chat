dd7571ce8fc3882feaa1398436375bc7243055cd
Salnikov <salnikov@100sp.ru>
2021-09-24 16:22:06 +1000

    Previous home task. Move code from __init__ files to separate modules

---
+++ jimmy/include/decorators/log.py
@@ -0,0 +1,26 @@
+import traceback
+import logging
+from functools import wraps
+
+
+def log(func):
+    @wraps(func)
+    def debug_log(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+
+        logger: logging.Logger = self.logger
+        # Get current level
+        level = logger.getEffectiveLevel()
+        logger.setLevel(logging.DEBUG)
+
+        stack = traceback.extract_stack()
+        stack.reverse()
+        parent = stack[1]
+
+        self.logger.debug(f'Call: {func.__name__} from {parent.name}("{parent.filename}:{parent.lineno}")')
+
+        # Restore log level
+        logger.setLevel(level)
+        return result
+
+    return debug_log
