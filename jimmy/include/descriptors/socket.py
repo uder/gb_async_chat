c6a2e6a2237a7573547aba18fd3c39b872f1569f
Salnikov <salnikov@100sp.ru>
2021-10-06 16:19:06 +1000

    lesson10. task 2 Server socket descriptor

--- jimmy/include/descriptors/socket.py
+++ jimmy/include/descriptors/socket.py
@@ -26,3 +26,8 @@ class SocketDescriptorClient(SocketDescriptor):
     def validate_socket(self, s: socket) -> None:
         if not (s.family == AF_INET and s.type == SOCK_STREAM):
             raise ValueError("ERROR: wrong socket config")
+
+
+class SocketDescriptorServer(SocketDescriptor):
+    def validate_socket(self, s: socket) -> None:
+        pass
